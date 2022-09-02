411dbad8eac76d290ee0664c3a9112bba2ec379a
Petros Tsimvris <tsimvris@gmail.com>
2022-09-02 16:27:17 +0200

    delete button added Fixes #9

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {Bookings} from '../mock-bookings';
+import { Bookings } from '../mock-bookings';
+import { Booking } from '../booking';
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.component.html',
-  styleUrls: ['./bookings.component.css']
+  styleUrls: ['./bookings.component.css'],
 })
 export class BookingsComponent implements OnInit {
-
-  constructor() { }
-  bookings  = Bookings;
-  ngOnInit(): void {
+  constructor() {}
+  bookings = Bookings;
+  ngOnInit(): void {}
+  deleteBooking(booking: Booking): void {
+    let i = Bookings.indexOf(booking);
+    Bookings.splice(i, 1);
   }
-
 }
