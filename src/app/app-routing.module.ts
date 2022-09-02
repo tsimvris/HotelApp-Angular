5a0d082a07633b8bde0d99ed43bc6ba889d1f754
Petros Tsimvris <tsimvris@gmail.com>
2022-09-02 12:54:25 +0200

    create comp and add it to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookingsComponent } from './bookings/bookings.component';
+import { CreateBookingComponent } from './create-booking/create-booking.component';
 
 const routes: Routes = [
   { path: 'bookings', component: BookingsComponent },
+  { path: 'createBooking', component: CreateBookingComponent },
   {path:"", redirectTo:"/bookings", pathMatch:"full"}
 ];
 @NgModule({
