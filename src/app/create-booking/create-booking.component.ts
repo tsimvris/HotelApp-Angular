8860fec53644151cadaa6485e186ec01680d2da5
Petros Tsimvris <tsimvris@gmail.com>
2022-09-02 13:55:35 +0200

    added formControl Fixes #5

--- src/app/create-booking/create-booking.component.ts
+++ src/app/create-booking/create-booking.component.ts
@@ -1,21 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {Booking} from "../booking"
+import { Booking } from '../booking';
 @Component({
   selector: 'app-create-booking',
   templateUrl: './create-booking.component.html',
-  styleUrls: ['./create-booking.component.css']
+  styleUrls: ['./create-booking.component.css'],
 })
 export class CreateBookingComponent implements OnInit {
-
-  constructor() { }
-booking: Booking = {
-  id: 111,
-  name: "string",
-  roomNumber: 111,
-  startDate: new Date(),
-  endDate: new Date(),
-};
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  booking: Booking = {
+    id: 111,
+    name: 'Your Name',
+    roomNumber: 111,
+    startDate: new Date(),
+    endDate: new Date(),
+  };
+  ngOnInit(): void {}
 }
